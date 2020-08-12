0774ab29319a740a5aa3aeb9006f99bcc9172e82
daeungkim <daeungkim93@gmail.com>
2020-08-12 09:29:06 +0900

    update:ft_calloc함수 프로토타입 추가
    <description>
    ./src/gnl/get_next_line_utils.c에 선언되어있는 ft_calloc함수 프로토타입
    ./include/minishell.h헤더에 프로토 타입 추가
    =>문자열 동적할당시 null문자 처리할 필요 없기 때문
    <issue-number>

--- include/minishell.h
+++ include/minishell.h
@@ -6,7 +6,7 @@
 /*   By: dakim <dakim@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/08/10 16:30:38 by seunkim           #+#    #+#             */
-/*   Updated: 2020/08/11 15:04:16 by dakim            ###   ########.fr       */
+/*   Updated: 2020/08/12 09:28:55 by dakim            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,22 @@
 # include <sys/wait.h>
 # include <signal.h>
 
+# define ECHO_STR "echo"
+# define PWD_STR "cd"
+# define EXPORT_STR "export"
+# define UNSET_STR "unset"
+# define ENV_STR "env"
+# define EXIT_STR "exit"
+
+# define INITIAL_INT 0
+# define ECHO_INT 1
+# define PWD_INT 2
+# define EXPORT_INT 3
+# define UNSET_INT 4
+# define ENV_INT 5
+# define EXIT_INT 6
+
 int		get_next_line(int fd, char **line);
+void	*ft_calloc(size_t nelem, size_t elsize);
 
 #endif
