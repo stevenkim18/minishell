384c8122cbb478f65ab01f75987cad64fa9c4a0f
daeungkim <daeungkim93@gmail.com>
2020-08-15 16:07:48 +0900

    update:프로세스간 통신 수행 함수 구현
    <description>
    프로세스간 통신 수행하는 함수(ft_pipe_init ft_send_signal ft_get_signal)
    구현
    <issue-number>

--- src/utils/ft_handle_signal.c
+++ src/utils/ft_handle_signal.c
@@ -1,20 +1,42 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   ft_handle_signal.c                                 :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: dakim <dakim@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/08/12 16:04:27 by dakim             #+#    #+#             */
-/*   Updated: 2020/08/15 14:54:47 by dakim            ###   ########.fr       */
+/*   Created: 2020/08/15 14:06:02 by dakim             #+#    #+#             */
+/*   Updated: 2020/08/15 16:03:13 by dakim            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int				main(void)
+static int		g_fd[2];
+
+void			ft_pipe_init(void)
 {
-	ft_pipe_init();
-	ft_handle_command();
-	return (0);
+	while (true)
+	{
+		if (-1 != pipe(g_fd))
+			break ;
+	}
+}
+
+void			ft_send_signal(int signal)
+{
+	ft_putnbr_fd(signal, g_fd[WRITE]);
+	ft_putchar_fd('\n', g_fd[WRITE]);
+}
+
+int				ft_get_signal(void)
+{
+	char		*line;
+	int			return_value;
+
+	if (-1 == get_next_line(g_fd[READ], &line))
+		return (ENOMEM);
+	return_value = ft_atoi(line);
+	free(line);
+	return (return_value);
 }
