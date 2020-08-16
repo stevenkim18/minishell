142cb61491fa053a5eb3204849e4b77794fde262
daeungkim <daeungkim93@gmail.com>
2020-08-16 15:37:19 +0900

    etc:에러코드 저장 기능 테스트
    <description>
    에러코드 저장 기능 테스트
    <issue-number>
    770cb154c2e734407fa76f8aabd7b07f7eb64120

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: dakim <dakim@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/08/12 16:04:27 by dakim             #+#    #+#             */
-/*   Updated: 2020/08/16 15:27:00 by dakim            ###   ########.fr       */
+/*   Updated: 2020/08/16 15:37:00 by dakim            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,6 +29,8 @@ int				main(void)
 	else
 	{
 		printf("parent\n");
+		printf("error = %d\n", ft_handle_error(NO_ERROR, NULL));
+		printf("error = %d\n", ft_handle_error(NO_ERROR, NULL));
 		while (1)
 			;
 	}
