a9571070bb0ee8dfcf1faaecd2f41a9a39f2fdf7
daeungkim <daeungkim93@gmail.com>
2020-08-16 11:59:15 +0900

    etc:파이프 명령어 프로세스 테스트
    <description>
    파이프 명령어 들어온경우 손자 프로세스를 생성하는것이 유리한지
    자식프로세스를 여러개 생성하는게 나은지 테스트
    <issue-number>

--- src/main.c
+++ src/main.c
@@ -6,48 +6,51 @@
 /*   By: dakim <dakim@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/08/12 16:04:27 by dakim             #+#    #+#             */
-/*   Updated: 2020/08/15 17:37:16 by dakim            ###   ########.fr       */
+/*   Updated: 2020/08/16 11:59:04 by dakim            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+void		ft_leak_test(void)
+{
+	pid_t pid;
+
+	char *str = NULL;
+	pid = fork();
+	int parent;
+	wait(&parent);
+	if (pid == 0)
+	{
+		str = (char *)malloc(sizeof(char) * 2);
+		*str = '5';
+		*(str + 1) = 0;
+		printf("child = %s\n", str);
+
+		pid_t pid1;
+		int child;
+
+		pid1 = fork();
+		wait(&child);
+		if (pid1 == 0)
+		{
+			printf("grand child = %s\n", str);
+			exit(0);
+		}
+		exit(0);
+	}
+	printf("parent = %s\n", str);
+}
+
 int				main(void)
 {
-	ft_pipe_init();
+	// ft_open_pipe();
 	// ft_handle_command();
+	// ft_close_pipe();
 
-	// 파이프 재사용 테스트
-	// ft_send_signal(123);
-	// printf("%d\n", ft_get_signal());
-	// ft_send_signal(123);
-	// printf("%d\n", ft_get_signal());
-	// ft_send_signal(123);
-	// printf("%d\n", ft_get_signal());
-
-	// 프로세스 재사용 테스트
-	// int signal;
-	// pid_t pid;
-	// signal = ft_start_process(&pid);
-	// ft_handle_error(signal, NULL);
-	// if (pid == 0)
-	// {
-	// 	ft_end_process(127);
-	// }
-	// signal = ft_start_process(&pid);
-	// ft_handle_error(signal, NULL);
-	// if (pid == 0)
-	// {
-	// 	ft_end_process(0);
-	// }
-	// signal = ft_start_process(&pid);
-	// ft_handle_error(signal, NULL);
-	// if (pid == 0)
-	// {
-	// 	ft_end_process(ENOMEM);
-	// }
-
-
+	ft_leak_test();
+	while (1)
+		;
 
 	return (0);
 }
