ef884b1e536c0e00761450edf9ea40f1fe362919
daeungkim <daeungkim93@gmail.com>
2020-08-16 15:13:53 +0900

    update:ctrl입력 처리 및 테스트 수행
    <description>
    ctrl입력 처리 및 잘 작동하는지 테스트 수행
    <issue-number>

--- src/main.c
+++ src/main.c
@@ -6,34 +6,33 @@
 /*   By: dakim <dakim@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/08/12 16:04:27 by dakim             #+#    #+#             */
-/*   Updated: 2020/08/16 13:01:51 by dakim            ###   ########.fr       */
+/*   Updated: 2020/08/16 15:11:35 by dakim            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void			ft_handle_signal(int signal)
-{
-	if (SIGINT == signal)
-		printf("\nsignal = %d\n", signal);
-	else if (SIGCHLD == signal)
-		printf("\nend child signal = %d\n", signal);
-}
-
 int				main(void)
 {
-	signal(SIGINT, ft_handle_signal);
-	signal(SIGCHLD, ft_handle_signal);
-	// ft_open_pipe();
-	// ft_handle_command();
-	// ft_close_pipe();
+	ft_register_parent_signal();
+
 	pid_t pid;
 
 	ft_start_process(&pid);
-	ft_end_process(128, pid);
-	ft_start_process(&pid);
-	ft_end_process(128, pid);
-	ft_start_process(&pid);
-	ft_end_process(128, pid);
+	if (pid == 0)
+	{
+		printf("child\n");
+		while (1)
+			;
+
+	}
+	else
+	{
+		printf("parent\n");
+		while (1)
+			;
+	}
+
+	// ft_handle_command();
 	return (0);
 }
--- src/utils/ft_handle_signal.c
+++ src/utils/ft_handle_signal.c
@@ -5,10 +5,35 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: dakim <dakim@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/08/15 14:06:02 by dakim             #+#    #+#             */
-/*   Updated: 2020/08/16 12:57:32 by dakim            ###   ########.fr       */
+/*   Created: 2020/08/16 15:11:17 by dakim             #+#    #+#             */
+/*   Updated: 2020/08/16 15:11:45 by dakim            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+void			ft_handle_parent_signal(int signal)
+{
+	if (SIGCHLD == signal)
+	{
+		printf("exit\n");
+	}
+
+}
+
+void			ft_handle_child_signal(int signal)
+{
+	if (SIGINT == signal)
+		ft_end_process(130, 0);
+}
+
+void			ft_register_parent_signal(void)
+{
+	signal(SIGINT, ft_handle_parent_signal);
+	signal(SIGCHLD, ft_handle_parent_signal);
+}
+
+void			ft_register_child_signal(void)
+{
+	signal(SIGINT, ft_handle_child_signal);
+}
