7b4de69deed84e837b60acc90b6e0c681a86d3b7
daeungkim <daeungkim93@gmail.com>
2020-08-25 18:07:59 +0900

    bug:; |인경우 인덱스 카운팅 버그
    <description>
    ;나 |로 연결되어있는 명령어(echo "123" | echo 123 | echo 12)의 경우 첫번째에는 상관이 없지만
    두번째 부터 인텍스 카운팅이 이상하게 작동함
    <issue-number>

--- src/command/ft_route_command.c
+++ src/command/ft_route_command.c
@@ -6,7 +6,7 @@
 /*   By: dakim <dakim@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/08/20 18:05:05 by dakim             #+#    #+#             */
-/*   Updated: 2020/08/25 15:19:14 by dakim            ###   ########.fr       */
+/*   Updated: 2020/08/25 18:07:17 by dakim            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,11 @@ int			ft_test(const char *str, int *index)
 {
 	if (*str)
 	{
-		write(STDOUT, (str + *index), 1);
-		++(*index);
+		if (*(str + *index))
+		{
+			write(STDOUT, (str + *index), 1);
+			++(*index);
+		}
 		while (*(str + *index)
 		&& *(str + *index) != ';' && *(str + *index) != '|')
 		{
@@ -31,11 +34,11 @@ int			ft_test(const char *str, int *index)
 
 int			ft_check_echo_cd(const char *str, int *index, int i)
 {
-	*index += i;
 	if (ft_strnstr(str + i, ECHO_STR, ft_strlen(ECHO_STR))
 	|| ft_strnstr(str + i, ECHO_D, ft_strlen(ECHO_D))
 	|| ft_strnstr(str + i, ECHO_S, ft_strlen(ECHO_S)))
 	{
+		*index = i;
 		ft_trim_command(str, index, ECHO_STR);
 		ft_exec_process(ft_test, str, index);
 		return (0);
@@ -44,6 +47,7 @@ int			ft_check_echo_cd(const char *str, int *index, int i)
 	|| ft_strnstr(str + i, CD_D, ft_strlen(CD_D))
 	|| ft_strnstr(str + i, CD_S, ft_strlen(CD_S)))
 	{
+		*index = i;
 		ft_trim_command(str, index, CD_STR);
 		ft_exec_process(ft_test, str, index);
 		return (0);
@@ -53,11 +57,11 @@ int			ft_check_echo_cd(const char *str, int *index, int i)
 
 int			ft_check_export_env(const char *str, int *index, int i)
 {
-	*index += i;
 	if (ft_strnstr(str + i, EXPORT_STR, ft_strlen(EXPORT_STR))
 	|| ft_strnstr(str + i, EXPORT_D, ft_strlen(EXPORT_D))
 	|| ft_strnstr(str + i, EXPORT_S, ft_strlen(EXPORT_S)))
 	{
+		*index = i;
 		ft_trim_command(str, index, EXPORT_STR);
 		ft_exec_process(ft_test, str, index);
 		return (0);
@@ -66,6 +70,7 @@ int			ft_check_export_env(const char *str, int *index, int i)
 	|| ft_strnstr(str + i, ENV_D, ft_strlen(ENV_D))
 	|| ft_strnstr(str + i, ENV_S, ft_strlen(ENV_S)))
 	{
+		*index = i;
 		ft_trim_command(str, index, ENV_STR);
 		ft_exec_process(ft_test, str, index);
 		return (0);
@@ -83,6 +88,8 @@ int			ft_check_pwd_exit_unset(const char *str, int *index, int i)
 	|| ft_strnstr(str + i, PWD_D, ft_strlen(PWD_D))
 	|| ft_strnstr(str + i, PWD_S, ft_strlen(PWD_S)))
 	{
+		*index = i;
+		ft_trim_command(str, index, PWD_STR);
 		ft_exec_process(ft_handle_pwd, str, index);
 		return (0);
 	}
@@ -90,7 +97,7 @@ int			ft_check_pwd_exit_unset(const char *str, int *index, int i)
 	|| ft_strnstr(str + i, UNSET_D, ft_strlen(UNSET_D))
 	|| ft_strnstr(str + i, UNSET_S, ft_strlen(UNSET_S)))
 	{
-		*index += i;
+		*index = i;
 		ft_trim_command(str, index, UNSET_STR);
 		ft_exec_process(ft_test, str, index);
 		return (0);
@@ -113,7 +120,7 @@ void		ft_route_command(const char *str, int *index)
 		else if (!ft_check_pwd_exit_unset(str, index, i))
 			return ;
 		else
-			ft_exec_process(ft_handle_built_in, str, index);
+			ft_exec_process(ft_test, str, index);
 	}
 	else
 		ft_handle_error(ENOMEM, NULL);
