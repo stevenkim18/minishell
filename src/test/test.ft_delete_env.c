ee5742c454a144db7c73bd429db93a43391a9b40
daeungkim <daeungkim93@gmail.com>
2020-08-26 12:04:22 +0900

    update:환경변수 삭제 함수 생성
    <description>
    환경변수 삭제 함수 생성
    <issue-number>

--- src/test/test.ft_delete_env.c
+++ src/test/test.ft_delete_env.c
@@ -1,23 +1,22 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   test.ft_get_env.c                                  :+:      :+:    :+:   */
+/*   test.ft_delete_env.c                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: dakim <dakim@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/08/21 17:59:22 by dakim             #+#    #+#             */
-/*   Updated: 2020/08/26 11:57:34 by dakim            ###   ########.fr       */
+/*   Created: 2020/08/26 11:56:50 by dakim             #+#    #+#             */
+/*   Updated: 2020/08/26 12:03:43 by dakim            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 #include <criterion/criterion.h>
 
-Test(ft_get_env, basic)
+Test(ft_delete_env, basic)
 {
 	char command[1024];
-	ft_get_env("HOME", command);
-	cr_expect_str_eq(command, "/Users/dakim");
+	ft_delete_env("HOME");
 	ft_get_env("HOMsfsfsfsfsfsfdE", command);
 	cr_expect_eq(*command, 0);
 }
