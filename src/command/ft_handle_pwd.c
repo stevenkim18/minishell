d8879dfb8781872e0c1f4cdfd2bef4e378fbfa88
daeungkim <daeungkim93@gmail.com>
2020-08-26 16:42:58 +0900

    bug:pwd '|'출력문제 해결
    <description>
    pwd |인경우 출력이 되지 않아야하는데 출력이 됨
    <issue-number>

--- src/command/ft_handle_pwd.c
+++ src/command/ft_handle_pwd.c
@@ -6,7 +6,7 @@
 /*   By: dakim <dakim@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/08/17 18:23:46 by seunkim           #+#    #+#             */
-/*   Updated: 2020/08/26 08:36:57 by dakim            ###   ########.fr       */
+/*   Updated: 2020/08/26 16:42:41 by dakim            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,6 @@ int		ft_handle_pwd(const char *command, int *index)
 {
 	char	buff[1024];
 
-	ft_return_end(command, index);
 	getcwd(buff, 1024);
 	if (*(command + *index) == '|')
 		ft_send_str(buff, ft_get_data_pipe());
@@ -33,5 +32,6 @@ int		ft_handle_pwd(const char *command, int *index)
 		ft_putstr_fd(buff, STDOUT);
 		ft_putstr_fd(NEWLINE_STR, STDOUT);
 	}
+	ft_return_end(command, index);
 	return (NO_ERROR);
 }
