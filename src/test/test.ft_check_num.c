5498120be50e683e37f9de01ed871893556d3018
daeungkim <daeungkim93@gmail.com>
2020-08-27 15:18:10 +0900

    update:문자열이 숫자로만 구성되어있는지 판단하는 함수 작성
    <description>
    문자열이 숫자로만 구성되어 있는지 판단하는 함수 작성
    <issue-number>

---
+++ src/test/test.ft_check_num.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   test.ft_check_num.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dakim <dakim@student.42seoul.kr>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/08/27 15:07:05 by dakim             #+#    #+#             */
+/*   Updated: 2020/08/27 15:09:25 by dakim            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+#include <criterion/criterion.h>
+
+Test(ft_check_num, basic)
+{
+	char		*str = NULL;
+
+	str = "123123";
+	cr_expect_eq(ft_check_num(str), 0);
+	str = "123d123";
+	cr_expect_eq(ft_check_num(str), -1);
+}
