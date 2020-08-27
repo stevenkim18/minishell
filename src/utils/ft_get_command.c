f7ecaa8e47706afd18a676148718bf6622c4f294
daeungkim <daeungkim93@gmail.com>
2020-08-27 15:59:05 +0900

    bug:세그멘테이션 오류 해결
    <description>
    세그멘테이션 오류 해결
    <issue-number>

--- src/utils/ft_get_command.c
+++ src/utils/ft_get_command.c
@@ -6,7 +6,7 @@
 /*   By: dakim <dakim@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/08/25 17:24:01 by dakim             #+#    #+#             */
-/*   Updated: 2020/08/26 16:54:52 by dakim            ###   ########.fr       */
+/*   Updated: 2020/08/27 15:56:28 by dakim            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,13 +27,14 @@ void		ft_get_command(const char *str, char *command)
 	{
 		while (*(str + i) && (*(str + i) == '\'' || *(str + i) == '\"'))
 			++i;
-		while (*(str + i) && (*(str + i) != '\'' && *(str + i) != '\"'))
+		while (*(str + i) && j < 1023
+		&& (*(str + i) != '\'' && *(str + i) != '\"'))
 			command[j++] = *(str + i++);
 	}
 	else
 	{
-		while (*(str + i) && (*(str + i) != '\''
-		&& *(str + i) != '\"' && *(str + i) != ' '))
+		while (*(str + i) && j < 1023
+		&& (*(str + i) != '\'' && *(str + i) != '\"' && *(str + i) != ' '))
 			command[j++] = *(str + i++);
 	}
 	command[j] = 0;
