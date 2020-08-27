41f5a3ffe75aca3e92682386650164121961ea6b
daeungkim <daeungkim93@gmail.com>
2020-08-27 17:32:33 +0900

    bug:cd 실패시 OLDPWD변경 에러 수정
    <description>
    cd 실패시 OLDPWD변경 에러 수정
    <issue-number>

--- src/command/ft_handle_cd.c
+++ src/command/ft_handle_cd.c
@@ -6,7 +6,7 @@
 /*   By: dakim <dakim@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/08/24 15:39:09 by seunkim           #+#    #+#             */
-/*   Updated: 2020/08/27 17:25:47 by dakim            ###   ########.fr       */
+/*   Updated: 2020/08/27 17:31:13 by dakim            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,19 +16,21 @@ int		ft_handle_cd(const char *command, int *index)
 {
 	char		dir[1024];
 	char		path[1024];
+
 	ft_get_command(command + *index, dir);
 	ft_check_home_dir(dir);
 	ft_return_end(command, index);
 	if (!*dir)
 		ft_get_env("HOME", dir);
 	getcwd(path, 1024);
-	ft_set_env("OLDPWD", path);
 	if (chdir(dir))
 	{
 		ft_handle_error(ENODIR, dir);
 		return (ENODIR);
 	}
+	ft_set_env("OLDPWD", path);
 	getcwd(path, 1024);
 	ft_set_env("PWD", path);
 	return (NO_ERROR);
 }
+// TODO cd 사용시 환경변수 변하는지 테스트 하지 않았음
