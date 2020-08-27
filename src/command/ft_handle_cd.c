bcd74c00d5b6186ea111def66f7684dd6d561516
daeungkim <daeungkim93@gmail.com>
2020-08-27 17:25:50 +0900

    update:cd 명령어 사용시 PWD 및 OLDPWD 변하도록 로직 변경
    <description>
    명령어 사용시 PWD 및 OLDPWD 환경변수 변하도록 로직 변경
    <issue-number>

--- src/command/ft_handle_cd.c
+++ src/command/ft_handle_cd.c
@@ -6,7 +6,7 @@
 /*   By: dakim <dakim@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/08/24 15:39:09 by seunkim           #+#    #+#             */
-/*   Updated: 2020/08/26 10:28:29 by dakim            ###   ########.fr       */
+/*   Updated: 2020/08/27 17:25:47 by dakim            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,16 +15,20 @@
 int		ft_handle_cd(const char *command, int *index)
 {
 	char		dir[1024];
-
+	char		path[1024];
 	ft_get_command(command + *index, dir);
 	ft_check_home_dir(dir);
 	ft_return_end(command, index);
 	if (!*dir)
 		ft_get_env("HOME", dir);
+	getcwd(path, 1024);
+	ft_set_env("OLDPWD", path);
 	if (chdir(dir))
 	{
 		ft_handle_error(ENODIR, dir);
 		return (ENODIR);
 	}
+	getcwd(path, 1024);
+	ft_set_env("PWD", path);
 	return (NO_ERROR);
 }
